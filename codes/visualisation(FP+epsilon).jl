1eddd144f15f4f3558bf46adc44cd296ad08865e
An Duleabom <duleabom.an@jku.at>
2020-10-06 15:31:46 +0200

    a plot for one instance added

--- codes/visualisation(FP+epsilon).jl
+++ codes/visualisation(FP+epsilon).jl
@@ -2,6 +2,25 @@ using DelimitedFiles,DataFrames,PlotlyJS,RDatasets,Colors
 
 f= readdlm("/home/ak121396/Desktop/triflp_Y/01/05_010_01.txtFPep_2hr_Y_.csv")
 x1=f[:,1]; y1=f[:,2];z1=f[:,3]
+
+#########################  Plot solutions of 1 instance  #####################
+function Plot_solution()
+    rw = [10 5 7; 8 5 13; 2 7 4]
+    trace1 = scatter3d(;x=rw[:,1],y=rw[:,2], z=rw[:,3], mode="markers",
+                        marker=attr(color="#1f77b4", size=5, symbol="circle",
+                                    line=attr(color="rgb(44, 160, 44)", width=0)),
+                        line=attr(color="#1f77b4", width=1))
+    layout = Layout(autosize=false, width=500, height=500,
+                    margin=attr(l=0, r=0, b=0, t=65))
+    plot([trace1], layout) # trace2,trace3
+end
+Plot_solution()
+
+
+
+
+
+
 ##########################     Visualisation       ###########################
 nms = ["GroupFP + ϵ-constraint"]
 title = "GroupFP + ϵ-constraint size20_40" #this is the caption appearing in the figure
