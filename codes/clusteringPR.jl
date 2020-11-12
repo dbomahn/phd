5e50db66eb3bda739c53956b982359ee84aecf35
An Duleabom <duleabom.an@jku.at>
2020-11-12 17:15:30 +0100

    clustering

--- codes/clusteringPR.jl
+++ codes/clusteringPR.jl
@@ -1,4 +1,4 @@
-using DataStructures,DelimitedFiles,DataFrames,LinearAlgebra,Clustering
+using DataStructures,DelimitedFiles,DataFrames,LinearAlgebra,Clustering,StatsBase
 
 mutable struct Data
     dvar::String; dtfile::String
@@ -38,10 +38,28 @@ mutable struct Data
 end
 
 # paths = ("/home/ak121396/Desktop/Bensolve_KP/RoundDown/X/", "/home/ak121396/Desktop/Bensolve_KP/data/")
-dvfile = readdir(paths[1]);  ins = readdir(paths[3]);
+# dvfile = readdir(paths[1]);  ins = readdir(paths[3]);
 i=13
+pathY = "E:\\Bensolve_KP\\Y\\"; objfile = readdir(pathY);
 pathY= "/home/ak121396/Desktop/Bensolve_KP/Y/"; objfile = readdir(pathY);
 # kp = Data(paths[1]*file[i],paths[2]*ins[i])
-points = transpose(readdlm(pathY*objfile[i])[:,2:end])
+objs = readdlm(pathY*objfile[i])[:,2:end]
+ind = findall(i-> -1 in LB[i,:]  , 1:size(LB)[1])
+for i=1:3
 
-dbscan(points, 0.05, min_neighbors = 3, min_cluster_size = 20)
+    push!(LB, deleteat!(objs[:,i], ind[j] for j=1:length(ind)) )
+end
+
+LB = hcat(deleteat!(objs[:,1],ind[j] for j=1:length(ind)),deleteat!(objs[:,2],ind[j] for j=1:length(ind)),
+    deleteat!(objs[:,3],ind[j] for j=1:length(ind)) )
+maxobj = [maximum(LB[:,i]) for i=1:3]; minobj = [minimum(LB[:,i]) for i=1:3];
+radius = round(Int,mean(maxobj-minobj)/length(LB[:,1]))
+points = transpose(LB)
+LBcluster = dbscan(points, radius, min_neighbors = 1, min_cluster_size = 1)
+
+
+
+LBres = kmeans(points, 10;)# maxiter=200, display=:iter
+
+scatter(,LBres.Petalwidth, marker_z=LBres.assignments,
+        color=:lightrainbow, legend=true)
--- codes/visualisation(FP+epsilon).jl
+++ codes/visualisation(FP+epsilon).jl
@@ -1,5 +1,5 @@
-using DelimitedFiles,DataFrames,PlotlyJS,RDatasets,Colors
-
+using PlotlyJS,RDatasets,Colors
+# DelimitedFiles,DataFrames,
 f= readdlm("/home/ak121396/Desktop/triflp_Y/01/05_010_01.txtFPep_2hr_Y_.csv")
 x1=f[:,1]; y1=f[:,2];z1=f[:,3]
 
@@ -15,10 +15,6 @@ function Plot_solution()
     plot([trace1], layout) # trace2,trace3
 end
 Plot_solution()
-
-
-
-
 
 
 ##########################     Visualisation       ###########################
@@ -53,77 +49,10 @@ layout = Layout(width=800, height=600, autosize=true, title=title,
                            aspectmode = "manual"))
 PlotlyJS.plot(data, layout)
 
-############################  tri-obj Feasibility Pump  ############################
-nms = ["Feasibility Pump "]
-title = "Feasibility Pump " #in cuboid"
-colors = [RGB(0.89, 0.1, 0.1),RGB(0.28, 0.68, 0.3),RGB(0.4,0.4,1), RGB(0.59,0,0.84),RGB(0,0.56,0.56)]
-data = GenericTrace[]
-trace2 = PlotlyJS.scatter3d(;name=title, mode="markers",
-                   marker_size=3, marker_color=colors[1], marker_line_width=0,
-                   x=Px, y=Py, z=Pz)
-# trace2 = PlotlyJS.scatter3d(;name="FP+LS", mode="markers",
-#                    marker_size=3, marker_color=colors[2], marker_line_width=0,
-#                    x=PFx, y=PFy, z=PFz)
-push!(data, trace2) #,trace2)
-# cluster = mesh3d(;color=colors[1], opacity=0.3, x=x1, y=y1, z=z1) #for masch
-# push!(data,cluster)
-
-layout = Layout(width=800, height=600, autosize=true, title=title,
-                scene=attr(xaxis=attr(gridcolor="rgb(255, 255, 255)",
-                                      zerolinecolor="rgb(255, 255, 255)",
-                                      showbackground=true,
-                                      backgroundcolor="rgb(230, 230,230)"),
-                           yaxis=attr(gridcolor="rgb(255, 255, 255)",
-                                       zerolinecolor="rgb(255, 255, 255)",
-                                       showbackground=true,
-                                       backgroundcolor="rgb(230, 230,230)"),
-                           zaxis=attr(gridcolor="rgb(255, 255, 255)",
-                                       zerolinecolor="rgb(255, 255, 255)",
-                                       showbackground=true,
-                                       backgroundcolor="rgb(230, 230,230)"),
-                           aspectratio=attr(x=1, y=1, z=1),
-                           aspectmode = "manual"))
-PlotlyJS.plot(data, layout)
-
-
-################################   FP ver2    ####################################
-nms = ["FP+ϵ ver2"] #this is the caption appearing in the figure
-title = "FP+ϵ ver2"
-colors = [RGB(0.89, 0.1, 0.1),RGB(0.28, 0.68, 0.3),RGB(0.4,0.4,1), RGB(0.59,0,0.84),RGB(0,0.56,0.56)]
-data = GenericTrace[]
-trace4 = PlotlyJS.scatter3d(;name=title, mode="markers",
-                   marker_size=3, marker_color=colors[5], marker_line_width=0,
-                   x=Tx, y=Ty, z=Tz)
-# trace2 = PlotlyJS.scatter3d(;name="FP+LS", mode="markers",
-#                    marker_size=3, marker_color=colors[2], marker_line_width=0,
-#                    x=PFx, y=PFy, z=PFz)
-push!(data, trace4) #,trace2)
-# cluster = mesh3d(;color=colors[1], opacity=0.3, x=x1, y=y1, z=z1) #for masch
-# push!(data,cluster)
-
-layout = Layout(width=800, height=600, autosize=true, title=title,
-                scene=attr(xaxis=attr(gridcolor="rgb(255, 255, 255)",
-                                      zerolinecolor="rgb(255, 255, 255)",
-                                      showbackground=true,
-                                      backgroundcolor="rgb(230, 230,230)"),
-                           yaxis=attr(gridcolor="rgb(255, 255, 255)",
-                                       zerolinecolor="rgb(255, 255, 255)",
-                                       showbackground=true,
-                                       backgroundcolor="rgb(230, 230,230)"),
-                           zaxis=attr(gridcolor="rgb(255, 255, 255)",
-                                       zerolinecolor="rgb(255, 255, 255)",
-                                       showbackground=true,
-                                       backgroundcolor="rgb(230, 230,230)"),
-                           aspectratio=attr(x=1, y=1, z=1),
-                           aspectmode = "manual"))
-PlotlyJS.plot(data, layout)
-
-
-using PlotlyJS,RDatasets,Colors
-f2= readdlm("/home/ak121396/Desktop/triflp_Y/01/05_010_01.txtep_2hr_Y.csv")
-epx=f2[:,1]; epy=f2[:,2];epz=f2[:,3]
-nms = ["ϵ-constraint"]
-title = "ϵ-constraint_size5_10" #in cuboid"
+###############################  Clustering  #################################
+epx=points[1,:]; epy=points[2,:]; epz=points[3,:]
+nms = ["Clustering LBsets"]
+title = "Clustering LBsets"
 colors = [RGB(0.89, 0.1, 0.1),RGB(0.28, 0.68, 0.3),RGB(0.4,0.4,1), RGB(0.8, 0.40, 0)]
 data = GenericTrace[]
 trace3 = PlotlyJS.scatter3d(;name=title, mode="markers",
