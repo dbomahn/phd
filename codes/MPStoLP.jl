6335730239ab2b1d519535929aacc572ee7dffc2
An Duleabom <duleabom.an@jku.at>
2021-01-28 17:01:08 +0100

    mps format to lp

--- codes/MPStoLP.jl
+++ codes/MPStoLP.jl
@@ -1,13 +1,12 @@
 using JuMP,MathOptFormat,CPLEX
+
 jump_model = Model()
 mof_model = MathOptFormat.MPS.Model()
-
-cd("../../instances/MIPLIB/")
-MOI.read_from_file(mof_model, "n3div36.mps")
-
+# cd("../../instances/MIPLIB/")
+MOI.read_from_file(mof_model, "C:\\Users\\AK121396\\Downloads\\n3div36.mps")
 MOI.copy_to(backend(jump_model), mof_model)
-
-
+# optimize!(jump_model, with_optimizer=CPLEX.Optimizer)
+print(jump_model)
 
 
 function writemodel(model::Model, filename::String)
@@ -24,3 +23,5 @@ function writemodel(model::Model, filename::String)
      MOI.write_to_file(file, filename)
      println("wrote model to ", filename)
 end
+
+writemodel(jump_model,"C:\\Users\\AK121396\\Downloads\\copymodel.lp")
