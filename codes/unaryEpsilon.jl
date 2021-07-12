62f66009bc6ae14141a19f98f8c1a0c1170b6072
An Duleabom <duleabom.an@jku.at>
2021-07-12 14:54:51 +0200

    unary epsilon indicator

---
+++ codes/unaryEpsilon.jl
@@ -0,0 +1,8 @@
+# cd("/home/ak121396//Downloads/performance_indi/indicators_linux/")
+cd("C:\\Users\\AK121396\\Downloads\\performance_indi\\indicators_win\\")
+
+run(pipeline(`C:\\Users\\AK121396\\Julia 1.5.2\\bin\\julia.exe`))
+
+run(pipeline(`./hv -r "2 2 2" 1 1 1`))
+
+run(mycommand)
