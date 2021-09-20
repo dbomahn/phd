b487cd28a101416a173b96b3132d6f43950e41b8
An Duleabom <dbomahn@gmail.com>
2021-09-20 15:53:38 +0200

    update

--- MESS.jl
+++ MESS.jl
@@ -1,31 +1,11 @@
 using DataFrames,DelimitedFiles,JuMP,LinearAlgebra
-dir = "/home/ak121396/Desktop/instances/PublicInstances/"
-data = readdlm(dir*ins[1], ';')
-dir = "C:/Users/AK121396/Desktop/phd/toy.dzn"
-data = readdlm(dir, ';')
 
-fdata = []
-for i in [1,2,size(data)[1]-1]
-    if occursin("=", data[i])==true
-        dt = parse(Int,split(data[i],"= ")[2])
-        push!(fdata, dt)
-    end
-end
-for i in [3,4,5]
-    if occursin("=", data[i])==true
-        dt = split(data[i],"= ")[2]
+@show file = ARGS[1][1:end-4]
 
-        push!(fdata, dt)
-    end
-end
-sc = zeros(Int,(fdata[2],fdata[1]))
-
-for i=6:(5+fdata[2])
-    tmp = split(data[i],r"[|,]")
-    for j=2:1+fdata[1]
-        sc[i-5,j-1] = parse(Int,tmp[j])
-    end
-end
+# dir = "/home/ak121396/Desktop/instances/PublicInstances/"
+# data = readdlm(dir*ins[1], ';')
+file = "C:/Users/AK121396/Desktop/phd/toy.dzn"
+# data = readdlm(dir, ';')
 
 mutable struct Data
     filepath::String; W::Int; S::Int; C::Array{}; F::Array{}; G::Array{};
@@ -72,7 +52,7 @@ mutable struct Data
                 SC[i-5,j-1] = parse(Int,tmp[j])
             end
         end
-        SC = reshape(SC, (1,S*W))
+        SC = reshape(transpose(SC), (1,S*W))
 
         tm = split(data[size(data)[1]],r"[|]")
         IP = []
@@ -85,37 +65,32 @@ mutable struct Data
     end
 end
 
-data = Data(dir*ins[1]) #Linux
-data = Data(dir) #Windows
+data = Data(file)
 
-###########################   Mathematical Model  #############################
+
 M = 9999999999999999
-wlp = Model() #GLPK.Optimizer
-@variable( wlp, y[1:data.W] ) #,binary=true
-@variable( wlp, x[1:data.S,1:data.W])
-@variable( wlp, z[1:data.S,1:data.W])
-for w=1:data.W
-    @constraint( wlp, sum(x[s,w] for s=1:data.S) <= data.C[w] )
-end
-for s=1:data.S
-    @constraint( wlp, sum(x[s,w] for w=1:data.W) <= data.G[s])
-end
-for w=1:data.W
-    @constraint( wlp, sum(x[s,w] for s=1:data.S) <= (M*y[w]) )
-end
-for w=1:data.W
-    for i=1:data.IC
-        @constraint( wlp, x[data.IP[i][1],w] <= M*z[data.IP[i][1],w] )
-        @constraint( wlp, x[data.IP[i][2],w] <= M*(1-z[data.IP[i][1],w]) )
-    end
-end
-@objective( wlp, Min, sum(data.F[w]*y[w] for w=1:data.W) + sum(sum(data.SC[s,w]*x[s,w] for w=1:data.W) for s=1:data.S) )
+###########################   Mathematical Model  #############################
+# wlp = Model() #GLPK.Optimizer
+# @variable( wlp, y[1:data.W] ) #,binary=true
+# @variable( wlp, x[1:data.S,1:data.W])
+# @variable( wlp, z[1:data.S,1:data.W])
+# for w=1:data.W
+#     @constraint( wlp, sum(x[s,w] for s=1:data.S) <= data.C[w] )
+# end
+# for s=1:data.S
+#     @constraint( wlp, sum(x[s,w] for w=1:data.W) <= data.G[s])
+# end
+# for w=1:data.W
+#     @constraint( wlp, sum(x[s,w] for s=1:data.S) <= (M*y[w]) )
+# end
+# for w=1:data.W
+#     for i=1:data.IC
+#         @constraint( wlp, x[data.IP[i][1],w] <= M*z[data.IP[i][1],w] )
+#         @constraint( wlp, x[data.IP[i][2],w] <= M*(1-z[data.IP[i][1],w]) )
+#     end
+# end
+# @objective( wlp, Min, sum(data.F[w]*y[w] for w=1:data.W) + sum(sum(data.SC[s,w]*x[s,w] for w=1:data.W) for s=1:data.S) )
 ################################  convert to vlp file  ###############################################
-obj = 1
-objnz = n*obj-(count(iszero,coef)+count(iszero,coef2)+count(iszero,coef3))
-NZ = length(B)-count(iszero,B)
-
-
 
 A1 = zeros(Int,data.W,data.S*data.W+data.W+data.W*data.S)
 for w=1:data.W
@@ -140,14 +115,16 @@ end
 A41= zeros(Int,data.IC*data.W,data.S*data.W+data.W)
 A42 = zeros(Int,data.IC*data.W,data.S*data.W)
 A51 = copy(A41); A52 =copy(A42)
-for w=1:data.W
-    for i=1:data.IC
-        A41[w, data.W*(data.IP[i][1]-1)+w] = 1
-        A42[w, data.W*(data.IP[i][1]-1)+w] = -M
+for i=1:data.IC
+    for w=1:data.W
+        k = data.W*(i-1)+w
+        A41[k, data.W*(data.IP[i][1]-1)+w] = 1
+        A42[k, data.W*(data.IP[i][1]-1)+w] = -M
     end
-    for i=1:data.IC
-        A51[w, data.W*(data.IP[i][2]-1)+w] = 1
-        A52[w, data.W*(data.IP[i][2]-1)+w] = M
+    for w=1:data.W
+        k = data.W*(i-1)+w
+        A51[k, data.W*(data.IP[i][2]-1)+w] = 1
+        A52[k, data.W*(data.IP[i][2]-1)+w] = M
     end
 end
 
@@ -157,8 +134,8 @@ P = hcat(data.SC,transpose(data.F), zeros(Int, (1,data.S*data.W)) )
 
 B = [A1;A2;A3;A4;A5]
 m,n = size(B)
-nz = count(i->(i==0),B)
-objnz = count(i->(i==0),P)
+nz = count(i->(i!=0),B)
+objnz = count(i->(i!=0),P)
 RHS = [data.C;data.G;zeros(Int,(data.W+data.W*data.IC,1));ones(Int,(data.W*data.IC,1))*M]
 
 wholearray=[];
@@ -180,13 +157,23 @@ for i=1:length(P)
 end
 
 for j=1:m
-    push!(wholearray,("i",j,"u",RHS[j]))
+    if data.W<j<data.W+data.S+1
+        push!(wholearray,("i",j,"s",RHS[j]))
+    else
+        push!(wholearray,("i",j,"u",RHS[j]))
+    end
 end
 
 
 push!(wholearray,"e")
+
+ins = open("/home/k2g00/k2g3475/mess/vlp/"*file[end-9:end-4]*".vlp","a")
+# ins = open("C:/Users/AK121396/Desktop/mess.vlp","a")
+
 writedlm(ins,wholearray)
 close(ins)
-end
 
-sizeof(wholearray)
+for i=1:length(wholearray)
+    println(wholearray[i])
+end
+wholearray
