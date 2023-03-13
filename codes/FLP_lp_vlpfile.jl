1aa4d28533574853d16626faac8bed7134c7cf1a
dbomahn <33522148+dbomahn@users.noreply.github.com>
2023-03-13 16:43:05 +0100

    Update and rename FLP_lpfile.jl to FLP_lp_vlpfile.jl

--- codes/FLP_lp_vlpfile.jl
+++ codes/FLP_lp_vlpfile.jl
@@ -111,90 +111,108 @@ for idx=1:1#20
     # print(flp);
     # writemodel(flp,dir1[1:end-10]*"/LP/"*files[idx][1:end-4]*".lp")
 end
-1
-        # For Bensolve vlp format
-        # for x=1:length(xval)
-        #     print(xval[x],"\n")
-        # end
-#
-#
-#         #Parray
-#         P = zeros(3,i+j+(i*j))
-#
-#         for y=1:i
-#             P[1,y] = fixcost[y]
-#         end
-#         for y=1:j
-#             P[2,i+y] = -demand[y]
-#         end
-#         for y=1:i*j
-#             P[3,i+j+y] = cost[y]
-#         end
-#
-#         # Barray
-#         # 1st constraint
-#         B = zeros(j+i*j,(i*j)+i+j)
-#         for x=1:j
-#             B[x,i+x]= -1
-#             for y=1:i
-#                 B[x,i+j+j*(y-1)+x] = 1
-#            end
-#         end
-#         # 2nd constraint
-#         for x=1:i
-#             for y=1:j
-#                 B[j*x+y,x] = -1
-#                 B[j*x+y,i+j+j*(x-1)+y] = 1
-#             end
-#         end
-#
-#         NZ = (i+1)*j+(2*i*j)
-#         objNZ = j+i+(i*j)
-#         ins = open(dir3*"/"*files[idx]*".vlp","a")
-#         wholearray=[];
-#         arr=["p vlp min",(i*j)+j,(j*i)+i+j,NZ,3,objNZ]
-#         push!(wholearray,arr)
-#
-#         # Parray
-#         for k=1:i
-#             push!(wholearray,("o",1,k,fixcost[k]))
-#         end
-#
-#         for k=1:j
-#             push!(wholearray,("o",2,i+k,-demand[k]))
-#         end
-#
-#         for k=1:i*j
-#             push!(wholearray,("o",3,i+j+k,cost[k]))
-#         end
-#
-#         for x=1:(i*j)+j
-#            for y=1:(i*j)+j+i
-#                if (B[x,y]!=0)
-#                    push!(wholearray,("a",x,y,Int128(B[x,y])))
-#                end
-#            end
-#         end
-#
-#         for x=1:i*j+j
-#             if x<=j
-#                 push!(wholearray,("i",x,"s",0))
-#             else
-#                 push!(wholearray,("i",x,"u",0))
-#             end
-#         end
-#
-#         for y=1:(j*i)+j+i
-#            push!(wholearray,("j", y,"d",0,1))
-#         end
-#         push!(wholearray,"e")
-#
-#         # println(ins,wholearray)
-#         writedlm(ins,wholearray)
-#         close(ins)
-#         print("It's done")
-#     end
-# end
+file = ARGS[1]
+f = readdlm(file)
+#facility_i, customer_j
+global i,j = filter!(a->typeof(a)==Int, f[1,:])
+# filter!(a->typeof(a)==Int, f[2,:])
+fixcost = []; capa = [];
+for k=2:i+1
+    fix,cp = filter!(a->typeof(a)==Int, f[k,:])
+    push!(fixcost,fix)
+    push!(capa,cp)
+end
+
+demand = filter!(a->typeof(a)==Int, f[2+i,:])
+
+cost = [];
+cost2 = [];
+a,b = size(f)
+for k=i+3:a
+    ct = filter!(a->typeof(a)==Int, f[k,:])
+    push!(cost2,ct)
+    for l=1:length(ct)
+        push!(cost,ct[l])
+    end
+end
+
+# For Bensolve vlp format
+#Parray
+P = zeros(3,i+j+(i*j))
+
+for y=1:i
+    P[1,y] = fixcost[y]
+end
+for y=1:j
+    P[2,i+y] = -demand[y]
+end
+for y=1:i*j
+    P[3,i+j+y] = cost[y]
+end
+
+# Barray
+# 1st constraint
+B = zeros(j+i*j,(i*j)+i+j)
+for x=1:j
+    B[x,i+x]= -1
+    for y=1:i
+        B[x,i+j+j*(y-1)+x] = 1
+   end
+end
+# 2nd constraint
+for x=1:i
+    for y=1:j
+        B[j*x+y,x] = -1
+        B[j*x+y,i+j+j*(x-1)+y] = 1
+    end
+end
+
+NZ = (i+1)*j+(2*i*j)
+objNZ = j+i+(i*j)
+fname = ARGS[1][end-12:end-4]
+ins = open("/home/k2g00/k2g3475/pastinstance/flp/vlp/"*"$fname"*".vlp", "a")
+wholearray=[];
+arr=["p vlp min",(i*j)+j,(j*i)+i+j,NZ,3,objNZ]
+push!(wholearray,arr)
+
+# Parray
+for k=1:i
+    push!(wholearray,("o",1,k,fixcost[k]))
+end
+
+for k=1:j
+    push!(wholearray,("o",2,i+k,-demand[k]))
+end
+
+for k=1:i*j
+    push!(wholearray,("o",3,i+j+k,cost[k]))
+end
+
+for x=1:(i*j)+j
+   for y=1:(i*j)+j+i
+       if (B[x,y]!=0)
+           push!(wholearray,("a",x,y,Int128(B[x,y])))
+       end
+   end
+end
+
+for x=1:i*j+j
+    if x<=j
+        push!(wholearray,("i",x,"s",0))
+    else
+        push!(wholearray,("i",x,"u",0))
+    end
+end
+
+for y=1:(j*i)+j+i
+   push!(wholearray,("j", y,"d",0,1))
+end
+push!(wholearray,"e")
+
+# println(ins,wholearray)
+writedlm(ins,wholearray)
+close(ins)
+
 
 # old version of write_to_file by using MathOptFormat
 # function writemodel(model::Model, filename::String)
